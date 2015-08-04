35c1e108e35e94548397590373c9db2797d0c698
Magnus Granberg <zorry@gentoo.org>
2015-08-04 23:02:34 +0200

    move tbc/pym to pym/tbc and pym/bin to bin

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ except ImportError:
 	from setuptools import setup
 
 def find_packages():
-        for dirpath, dirnames, filenames in os.walk('tbc/pym'):
-                if '__init__.py' in filenames:
-                        yield os.path.relpath(dirpath, 'tbc/pym')
+	for dirpath, dirnames, filenames in os.walk('pym'):
+		if '__init__.py' in filenames:
+			yield os.path.relpath(dirpath, 'pym')
+
 setup(
         version = os.path.split(os.path.abspath(__file__))[-2].split('-')[-1],
         packages = list(find_packages()),
-        package_dir = {'': 'tbc/pym'},
+        package_dir = {'tbc': 'pym'},
         name="tbc",
         author='Zorry',
         author_email='tinderbox-cluster@gentoo.org',
