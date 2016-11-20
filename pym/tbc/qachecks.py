cc61c64c0c2bfb3bdfe32eb0030db2ab69483346
Magnus Granberg <zorry@gentoo.org>
2016-11-20 16:37:42 +0100

    ImportError: No module named 'repoman.checks.ebuilds'

--- pym/tbc/qachecks.py
+++ pym/tbc/qachecks.py
@@ -14,7 +14,7 @@ from portage import os, _encodings, _unicode_decode, _unicode_encode
 from portage.exception import DigestException, FileNotFound, ParseError, PermissionDenied
 from _emerge.Package import Package
 from _emerge.RootConfig import RootConfig
-from repoman.checks.ebuilds.checks import run_checks
+from repoman.modules.scan.ebuild.checks import run_checks
 from tbc.repoman import repoman_main
 from tbc.sqlquerys import get_configmetadata_info, get_config_info, get_setup_info
 import portage
