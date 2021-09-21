ee1e1227138eb6d1bff5c0685f03878240c77ac5
Magnus Granberg <zorry@gentoo.org>
2021-09-21 03:28:32 +0200

    Chmod on the repository so portage can read it after the git step
    Signed-off-by: Magnus Granberg <zorry@gentoo.org>

--- buildbot_gentoo_ci/steps/builders.py
+++ buildbot_gentoo_ci/steps/builders.py
@@ -335,7 +335,11 @@ class UpdateRepos(BuildStep):
             repository_data = yield self.gentooci.db.repositorys.getRepositoryByUuid(project_repository_data['repository_uuid'])
             repository_path = yield os.path.join(portage_repos_path, repository_data['name'])
             yield self.build.addStepsAfterCurrentStep([
-            steps.Git(repourl=repository_data['url'],
+                steps.ShellCommand(command=["chmod", "-R", "o+rX", repository_data['name']],
+                             workdir=portage_repos_path)
+            ])
+            yield self.build.addStepsAfterCurrentStep([
+                steps.Git(repourl=repository_data['url'],
                             mode='full',
                             submodules=True,
                             alwaysUseLatest=True,
